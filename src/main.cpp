5040934a70542e152a9c33e8af1f93ecfbee1d73
rsmith8917 <rsmith8917@gmail.com>
2024-08-26 16:27:27 -0400

    vscode

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello world!" << std::endl;
+class Player
+{
+public:
+    int x, y;
+    int speed;
+
+    void Move(int xa, int ya)
+    {
+        x += xa * speed;
+        y += ya * speed;
+    }
+};
+
+int main()
+{
+    Player player;
+    player.x = 5;
+    player.y = 3;
+    player.speed = 1;
+    player.Move(2, 1);
+    std::cout << player.x << std::endl;
 
     return 0;
 }
